46150896c15bdb52efac4476f054b7ba7aa80576
wangmeng <wangmeng08@sparkedu.com>
2024-07-06 23:54:52 +0800

    init: logger

--- src/BaseTrace.ts
+++ src/BaseTrace.ts
@@ -1,4 +1,101 @@
 import { BreadcrumbTypes } from "./type";
+// import log4js from "log4js";
+import * as log4js from "log4js";
+import * as path from "path";
+
+class Logger {
+  _logger: log4js.Logger | null;
+  logPathName: string;
+  config: {
+    type: string;
+    filename: any;
+    encoding: string;
+    layout: { type: string; pattern: string };
+    //split by day
+    pattern: string;
+    // ends as '.log'
+    keepFileExt: boolean;
+    //insert the date at the tail of name
+    alwaysIncludePattern: boolean;
+  };
+  constructor() {
+    this._logger = null;
+    this.logPathName = "logs";
+    this.config = {
+      type: "dateFile",
+      filename: path.resolve(this.logPathName + "/info.log"),
+      encoding: "utf-8",
+      layout: {
+        type: "pattern",
+        pattern: "%d %p %h %m",
+      },
+      //split by day
+      pattern: "yyyy-MM-dd",
+      // ends as '.log'
+      keepFileExt: true,
+      //insert the date at the tail of name
+      alwaysIncludePattern: true,
+    };
+  }
+
+  init() {
+    //define and configure logger
+    log4js.configure({
+      appenders: {
+        default: this.config,
+        //debug appender
+        debugAppender: {
+          ...this.config,
+          filename: path.resolve(this.logPathName + "/debug.log"),
+        },
+        //debug filter
+        debugFilter: {
+          type: "logLevelFilter",
+          appender: "debugAppender", // assign a specific appender
+          level: "debug", // lowest log level to catch
+          maxLevel: "debug", //highest log level to catch
+        },
+        //info filter
+        infoFilter: {
+          type: "logLevelFilter",
+          appender: "default",
+          level: "info",
+          maxLevel: "info",
+        },
+        //warn filter
+        warnFilter: {
+          type: "logLevelFilter",
+          appender: "default",
+          level: "warn",
+          maxLevel: "warn",
+        },
+        errorAppender: {
+          ...this.config,
+          filename: path.resolve(this.logPathName + "/error.log"),
+        },
+        //error filter
+        errorFilter: {
+          type: "logLevelFilter",
+          appender: "errorAppender",
+          level: "error",
+          maxLevel: "fatal",
+        },
+      },
+
+      categories: {
+        default: {
+          //default is a must
+          appenders: ["infoFilter", "warnFilter", "errorFilter", "debugFilter"],
+          level: "debuug",
+        },
+      },
+    });
+
+    //get logger，无category传参，进入default配置中
+    this._logger = log4js.getLogger();
+    this._logger.level = "debug";
+  }
+}
 
 export const TraceDataTypes = {
   LOG: "LOG",
@@ -59,9 +156,7 @@ export class BaseLog implements BaseTraceInterface {
     // if (JS_BRIDE)
     // if (IS_NATIVE)
     // if (IS_ELECTRON)
-    if (document) {
-      console.log(log);
-    }
+    console.log(log);
   }
 
   protected saveBreadcrumb(data: TraceDataLog) {
